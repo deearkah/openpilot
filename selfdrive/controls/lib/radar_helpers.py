4794da8f20552b41a1dfd8d9028dd69d775e0ca4
kegman <8837066+kegman@users.noreply.github.com>
2021-08-29 02:19:26 -0400

    Adjust radar_helpers.py to match vision for lead_msg

--- selfdrive/controls/lib/radar_helpers.py
+++ selfdrive/controls/lib/radar_helpers.py
@@ -128,15 +128,16 @@ class Cluster():
       "modelProb": model_prob,
       "radar": True,
       "aLeadTau": float(self.aLeadTau)
-    }
+    }  
+            
 
   def get_RadarState_from_vision(self, lead_msg, v_ego):
     return {
-      "dRel": float(lead_msg.xyva[0] - RADAR_TO_CAMERA),
-      "yRel": float(-lead_msg.xyva[1]),
-      "vRel": float(lead_msg.xyva[2]),
-      "vLead": float(v_ego + lead_msg.xyva[2]),
-      "vLeadK": float(v_ego + lead_msg.xyva[2]),
+      "dRel": float(lead_msg.x[0] - RADAR_TO_CAMERA),
+      "yRel": float(-lead_msg.y[0]),
+      "vRel": float(lead_msg.v[0] - v_ego),
+      "vLead": float(lead_msg.v[0]),
+      "vLeadK": float(lead_msg.v[0]),
       "aLeadK": float(0),
       "aLeadTau": _LEAD_ACCEL_TAU,
       "fcw": False,
