99293c1ef2999c3f1250f468e77fe53111230345
deearkah <108318227+deearkah@users.noreply.github.com>
2022-07-02 17:12:34 -0400

    Change some battery charge settings

--- selfdrive/kegman_conf.py
+++ selfdrive/kegman_conf.py
@@ -147,11 +147,11 @@ class kegman_conf():
         self.write_config(self.config)
 
     else:
-      self.config = {"cameraOffset":"0.06", "lastTrMode":"1", "battChargeMin":"60", "battChargeMax":"70", \
-                     "wheelTouchSeconds":"180", "accelerationMode":"1","battPercOff":"25", "carVoltageMinEonShutdown":"11800", \
+      self.config = {"cameraOffset":"0.06", "lastTrMode":"1", "battChargeMin":"45", "battChargeMax":"66", \
+                     "wheelTouchSeconds":"600", "accelerationMode":"1","battPercOff":"30", "carVoltageMinEonShutdown":"11800", \
                      "brakeStoppingTarget":"0.25", "tuneGernby":"1", "AutoHold":"0",\
                      "Kp":"-1", "Ki":"-1", "liveParams":"1", "leadDistance":"5", "deadzone":"0.0", \
-                     "1barBP0":"-0.1", "1barBP1":"2.25", "2barBP0":"-0.1", "2barBP1":"2.5", "3barBP0":"0.0", \
+                     "1barBP0":"-0.1", "1barBP1":"2.25", "2barBP0":"-0.25", "2barBP1":"2.5", "3barBP0":"-0.1", \
                      "3barBP1":"3.0", "1barMax":"2.1", "2barMax":"2.1", "3barMax":"2.1", \
                      "1barHwy":"0.4", "2barHwy":"0.3", "3barHwy":"0.1", \
                      "steerRatio":"-1", "steerRateCost":"-1", "slowOnCurves":"0", "Kf":"-1", \
